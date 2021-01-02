a0074d4bd46a78076faff549b6ccb2de056fbbe6
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-01-02 08:47:32 -0300

    Tela com JoptionPane

---
+++ Interface Gráfica/LugaresFavoritos/Tela/Tela1.java
@@ -0,0 +1,12 @@
+package Tela;
+import javax.swing.JOptionPane;
+
+public class Tela1 {
+ 
+    public static void main(String[] args) {
+
+        JOptionPane.showInputDialog("Nome completo:");
+        JOptionPane.showInputDialog("Idade:");
+        JOptionPane.showInputDialog("Peso:");
+    }
+}
