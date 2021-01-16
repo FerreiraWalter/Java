adc184257c6f4474a61dccbdf61a0845fd4c1133
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-01-16 10:37:41 -0300

    Inicio Ex001

---
+++ Cod3r/Collections/Exercicios/C001.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+import java.util.ArrayList;
+
+public class C001 {
+    
+    public static void main(String[] args) {
+
+        ArrayList<Cores> lista = new ArrayList<>();
+
+        Cores c1 = new Cores("Azul");
+        
+
+
+    }
+}
