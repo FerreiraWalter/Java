aaaf62c96ea9b1bf7706648538b25b2f3afac84a
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-02-09 16:53:25 -0300

    Exercicio Triangulo

---
+++ JavaUNIT/Triangulo.java
@@ -0,0 +1,25 @@
+package JavaUNIT;
+
+import java.util.Scanner;
+
+public class Triangulo {
+
+    public static double areaTriangulo(double base, double altura) {
+        double area = (base * altura) / 2;
+        return area;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Base: ");
+        double base = input.nextInt();
+        System.out.print("Altura: ");
+        double altura = input.nextInt();
+
+        double area = areaTriangulo(base, altura);
+        System.out.println("Área Triângulo: " + area);
+
+    }
+}
