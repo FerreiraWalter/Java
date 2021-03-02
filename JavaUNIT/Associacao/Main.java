3522bae0f1ebd8ac88373966916525ea162f35de
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-03-02 15:41:18 -0300

    Adição de coisas

--- JavaUNIT/Associacao/Main.java
+++ JavaUNIT/Associacao/Main.java
@@ -1,11 +1,10 @@
 package JavaUNIT.Associacao;
 
 public class Main {
-    
     public static void main(String[] args) {
-        Pessoa p = new Pessoa();
+        Endereco end = new Endereco();
+        Pessoa p = new Pessoa("Walter", 19, "07299891514", end);
 
         System.out.println("Dados da Pessoa: " + p.imprimir());
-
     }
 }
