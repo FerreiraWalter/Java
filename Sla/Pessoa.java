55bfa02e7da7ab77643e0237b652d48f64edcce8
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-03-03 11:59:40 -0300

    Removendo linhas desnecessárias

--- Sla/Pessoa.java
+++ Sla/Pessoa.java
@@ -1,47 +1,11 @@
 package Sla;
 
-import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
 
 public class Pessoa {
     private String nome;
     private String cpf;
-    private String cargo;
     private Double salario;
-
-    //Getters e Setters
-	public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public Double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(Double salario) {
-        this.salario = salario;
-    }
 
     //Cadastrar Clientes
     public void cadastrarClientes() {
